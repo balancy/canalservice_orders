8fd64f2ee6855c735d3d01ecef440df08d2242b1
balancy <balancy@gmail.com>
2022-09-25 22:00:09 +0200

    Pass backend host address from docker-compose inside react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import AppContent from './components/AppContent';
 import logo from './logo.png';
 
 function App() {
-    const url = 'http://localhost:8080/'
+    const url = `${process.env.REACT_APP_BACKEND_HOST}:8080`
 
     return (
         <div className="App">
