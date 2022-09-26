61d869b90371fcc61036e134260a7d5cf49e9959
balancy <balancy@gmail.com>
2022-09-26 19:30:49 +0200

    Fix a bug with launching celery since there was no googlesheet id indicated.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,10 +108,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
